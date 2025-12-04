43957afde1b767d5a859d7022b1244e93d8d7d7b
Nemanja Manojlovic <contact@nemanjamanojlovic.com>
2025-12-04 15:23:21 +0100

    Pass the sources to the sources array not as individual arguments

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,10 +7,12 @@ return {
 		local null_ls = require("null-ls")
 
 		null_ls.setup({
-			null_ls.builtins.completion.spell,
-      null_ls.builtins.formatting.stylua,
-			null_ls.builtins.formatting.prettier,
-			require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+			sources = {
+				null_ls.builtins.completion.spell,
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+			},
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Call code format method" })
