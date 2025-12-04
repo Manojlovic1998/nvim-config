977e2bd334b755a0353e5edcfc19fb2ac2e6b585
Nemanja Manojlovic <contact@nemanjamanojlovic.com>
2025-12-04 15:19:06 +0100

    Fix setup calling for telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,9 +11,18 @@ return {
           live_grep = {
             file_ignore_patterns = { "node_modules" },
           }
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {}
+          }
         }
-
       })
+
+
+      telescope.load_extension("ui-select")
+
+
       local builtin = require("telescope.builtin");
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope Live grep"})
@@ -21,17 +30,5 @@ return {
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
-    config = function ()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      })
-      require("telescope").load_extension("ui-select")
-    end
   },
 }
-
