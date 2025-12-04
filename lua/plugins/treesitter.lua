467d8ad24d4069f343ac2c748b399d4c1e1412e0
Nemanja Manojlovic <contact@nemanjamanojlovic.com>
2025-12-04 15:13:43 +0100

    Move treesitter config into config function

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,15 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
-	lazy = false,
-	build = ":TSUpdate",
-	ensure_installed = { "javascript", "markdown", "vim", "vimdoc", "lua" },
-	sync_install = false,
-	auto_install = true,
-	highlight = { enable = true },
-	indent = { enable = true },
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "javascript", "markdown", "vim", "vimdoc", "lua" },
+      sync_install = false,
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
